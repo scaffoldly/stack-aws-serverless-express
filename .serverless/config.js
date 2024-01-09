c7d54ce4f7edd7c51b3a2735c039a5c4082df3e4
Christian Nuss <cnuss@users.noreply.github.com>
2024-01-09 12:26:17 +0000

    deploy prep

--- .serverless/config.js
+++ .serverless/config.js
@@ -7,7 +7,7 @@ const packageJson = require('../package.json');
 module.exports['SERVICE_NAME'] = packageJson.name;
 module.exports['SERVICE_SLUG'] = packageJson.name;
 module.exports['STAGE_DOMAIN'] =
-  process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN;
+  `${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
 module.exports['API_GATEWAY_DOMAIN'] =
   `${process.env.CODESPACE_NAME}-3000.${module.exports['STAGE_DOMAIN']}`;
 module.exports['DEPLOY_ROLE'] = 'arn:aws:iam::000000000000:root';
