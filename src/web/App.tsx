506fe43ab6de7f5f5a6a02663723b38e4b77ee7c
Christian Nuss <cnuss@users.noreply.github.com>
2024-02-19 17:05:18 +0000

    update branding

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import serverlessLogo from '../../public/serverless.svg';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../public/vite.svg';
+import expressjsLogo from '../../public/expressjs.svg';
+import scaffoldlyLogo from '../../public/scaffoldly.svg';
 import './App.css';
 import { health } from '../lib/api';
 
@@ -15,28 +17,45 @@ function App(): React.JSX.Element {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://serverless.com" target="_blank">
+          <img src={serverlessLogo} className="logo" alt="Serverless logo" />
+        </a>
+        <a href="https://expressjs.com" target="_blank">
+          <img src={expressjsLogo} className="logo" alt="ExpressJS logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Serverless + Express + React</h1>
       <div className="card">
         <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/web/App.tsx</code> and save to test HMR
+          Edit <code>src/api/index.ts</code> and save to modify ESBuild+Express
+          API
+        </p>
+        <p>
+          Edit <code>src/web/App.tsx</code> and save to modify Vite+React
+          Frontend
+        </p>
+        <p>
+          Open{' '}
+          <a href="api/swagger.html" target="_blank">
+            Swagger
+          </a>{' '}
+          to explore this project's OpenAPI
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <p>
         API Version:
         <br />
         <code>{version}</code>
       </p>
+      <div>
+        <a href="https://scaffoldly.dev" target="_blank">
+          <img src={scaffoldlyLogo} className="logo" alt="Scaffoldly logo" />
+        </a>
+      </div>
     </>
   );
 }
