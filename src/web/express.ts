28de5265710eb1f4f1fec34a213f79db1e31d262
Christian Nuss <cnuss@users.noreply.github.com>
2024-03-03 13:46:49 -0500

    update webHandler caching config

--- src/web/express.ts
+++ src/web/express.ts
@@ -3,13 +3,17 @@ import path from 'path';
 import mime from 'mime-types';
 
 export function webHandler(): express.RequestHandler {
-  return express.static(path.join(__dirname, '..', '.react'), {
-    maxAge: '1d',
+  return express.static(path.join(__dirname, '..', '.angular', 'browser'), {
     setHeaders: (r, p) => {
       if (mime.lookup(p) === 'text/html') {
         r.setHeader(
           'Cache-Control',
           'no-store, max-age=0, private, must-revalidate',
+        );
+      } else {
+        r.setHeader(
+          'Cache-Control',
+          `max-age=${process.env.NODE_ENV !== 'production' ? 0 : 86400}`,
         );
       }
     },
