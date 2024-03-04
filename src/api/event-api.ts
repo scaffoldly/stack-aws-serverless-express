92090f7d186e054a6a842f1e16e0735c976cd16a
Christian Nuss <cnuss@users.noreply.github.com>
2024-03-04 02:14:10 +0000

    serverless-offline-resources eventbridge support

--- src/api/event-api.ts
+++ src/api/event-api.ts
@@ -1,4 +1,9 @@
-import { DynamoDBStreamEvent, SNSEvent, SQSEvent } from 'aws-lambda';
+import {
+  DynamoDBStreamEvent,
+  EventBridgeEvent,
+  SNSEvent,
+  SQSEvent,
+} from 'aws-lambda';
 import { Body, Controller, Header, Hidden, Post, Route, Tags } from 'tsoa';
 import { UserIdentityTable } from './db/user-identity';
 import { HttpError } from './internal/errors';
@@ -70,17 +75,17 @@ export class EventApi extends Controller {
     console.log('SNS Records: ', event.Records);
   }
 
-  // @Post('/eventbridge')
-  // public async eventbridgeEvent(
-  //   @Header('Host') host: string,
-  //   @Body() body: unknown,
-  // ): Promise<void> {
-  //   if (host !== 'events.amazonaws.com') {
-  //     throw new HttpError(403);
-  //   }
+  @Post('/eventbridge')
+  public async eventbridgeEvent(
+    @Header('Host') host: string,
+    @Body() body: unknown,
+  ): Promise<void> {
+    if (host !== 'events.amazonaws.com') {
+      throw new HttpError(403);
+    }
 
-  //   const event = body as EventBridgeEvent<unknown, unknown>;
+    const event = body as EventBridgeEvent<'Scheduled Event', unknown>;
 
-  //   console.log('EventBridge Event: ', event.detail);
-  // }
+    console.log('Received Scheduled Event: ', event.detail);
+  }
 }
