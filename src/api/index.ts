f913e12f1e37384308700172e25f20d4c0c9df14
Christian Nuss <cnuss@users.noreply.github.com>
2024-03-16 06:36:06 -1000

    tiny change

--- src/api/index.ts
+++ src/api/index.ts
@@ -34,7 +34,7 @@ export class Api extends Controller {
     @Request() httpRequest: EnrichedRequest,
   ): Promise<HealthResponse> {
     return {
-      name: "Testing 123",
+      name: "Testing 1234",
       version: packageJson.version,
       now: new Date(),
       hrefs: {
